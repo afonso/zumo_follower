4564ebe11746a555c4d204d174ed815e3b450357
afonso <afonso@yack.com.br>
2015-10-30 00:03:56 -0200

    update

--- seguidor_pololu.ino
+++ seguidor_pololu.ino
@@ -1,49 +1,42 @@
-int m11 = 7;
-int m12 = 6;
-int m21 = 3;
-int m22 = 2;
-int m1 = 8;
-int m2 = 5;
-int s1, s2;
-extern int v = 0;
+int m11 = 7; //pino 1 motor 1
+int m12 = 6; //pino 2 motor 1
+int m21 = 3; //pino 1 motor 2
+int m22 = 2; //pino 2 motor 2
+
+int m1 = 6; //pwm motor 1 - velocidade
+int m2 = 8; //pwm motor 2 - velocidade
+int s1 = 4; //sensor 1
+int s2 = 12; //sensor 2
 
 void setup() {
   pinMode(m11, OUTPUT);
   pinMode(m12, OUTPUT);
   pinMode(m21, OUTPUT);
   pinMode(m22, OUTPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  s1 = wb(readQD1(11), 2500);
-  s2 = wb(readQD2(10), 1500);
-  if(s1 && s1){
+  // debug
+  Serial.print(readQD(s1)); 
+  Serial.print(" : "); 
+  Serial.print(wb(readQD(s1), 1500)); 
+  Serial.print(" : "); 
+  Serial.print(readQD(s2)); 
+  Serial.print(" : "); 
+  Serial.println(wb(readQD(s2), 1500)); 
+  
+  boolean sen1 = wb(readQD(s1), 1500); //wb(valorSensor, precisao)
+  boolean sen2 = wb(readQD(s2), 1500);
+  if(sen1 && sen2){
     frente(255);
-  }else if(s1 && !s2){
+  }else if(sen1 && !sen2){
     direita(255);
-  }else if(!s1 && s2){
+  }else if(!sen1 && sen2){
     esquerda(255);
   }else{
-    frente(120);
+    frente(175);
   }
-
-  Serial.print(readQD1(11));
-  Serial.print(" : ");
-  Serial.println(readQD2(10));
-  /*
-  frente(255);
-  delay(2000);
-  tras(255);
-  delay(2000);
-  direita(255);
-  delay(2000);
-  esquerda(255);
-  delay(2000);
-  */
-  delay(10);
 }
 
 void tras(int v) {
@@ -98,31 +91,14 @@ void direita(int v) {
   delay(10);        
 }
 
-int readQD2(int s){
-  pinMode(s, OUTPUT);
-  digitalWrite(s, HIGH);  
+int readQD(int QRE1113_Pin){
+  pinMode( QRE1113_Pin, OUTPUT );
+  digitalWrite( QRE1113_Pin, HIGH );  
   delayMicroseconds(10);
-  pinMode(s, INPUT);
+  pinMode( QRE1113_Pin, INPUT );
 
   long time = micros();
-
-  //time how long the input is HIGH, but quit after 3ms as nothing happens after that
-  while (digitalRead(s) == HIGH && micros() - time < 3000); 
-  int diff = micros() - time;
-
-  return diff;
-}
-
-int readQD1(int s){
-  pinMode(s, OUTPUT);
-  digitalWrite(s, HIGH);  
-  delayMicroseconds(10);
-  pinMode(s, INPUT);
-
-  long time = micros();
-
-  //time how long the input is HIGH, but quit after 3ms as nothing happens after that
-  while (digitalRead(s) == HIGH && micros() - time < 3000); 
+  while (digitalRead(QRE1113_Pin) == HIGH && micros() - time < 3000); 
   int diff = micros() - time;
 
   return diff;
@@ -137,4 +113,5 @@ boolean wb(int v, int m) {
   }
  return i; 
 }
+
 
